a45501a96d4be5348e04613439621fdf3b31f050
mylu0x <229919353+mylu0x@users.noreply.github.com>
2026-03-27 14:43:05 +0900

    feat(text): add lang argument

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,6 @@
 use clap::{Parser, Subcommand};
 
-use crate::commands;
+use crate::commands::{self, text::Lang};
 
 #[derive(Debug, Parser)]
 #[command(version, about = env!("CARGO_PKG_DESCRIPTION"))]
@@ -13,17 +13,21 @@ struct Cli {
 enum Commands {    
     /// Generate dummy text.
     Text {
-        /// Number of words to generate (defaults to 16)
+        /// Number of words to generate
         #[arg(short, long, default_value_t = 16)]
         words: usize,
         
-        /// Number of texts to generate (defaults to 1)
+        /// Number of texts to generate
         #[arg(short, long, default_value_t = 1)]
         count: usize,
         
         /// Generate texts with words in random order
         #[arg(short, long)]
-        random: bool
+        random: bool,
+        
+        /// Language to generate texts
+        #[arg(short, long)]
+        lang: Option<Lang>
     }
 }
 
@@ -31,7 +35,7 @@ pub fn run() -> anyhow::Result<()> {
     let cli = Cli::parse();
     
     match &cli.command {
-        Commands::Text { words, count, random } => commands::text::run(*words, *count, *random)?
+        Commands::Text { words, count, random, lang } => commands::text::run(*words, *count, *random, lang.clone())?
     }
     
     Ok(())
--- src/commands/text.rs
+++ src/commands/text.rs
@@ -1,3 +1,5 @@
+use clap::ValueEnum;
+
 const WORD_SET_EN: &[&str] = &[
     "hello", "how", "are", "you", "today", "I", "am", "doing", "fine",
     "how", "about", "you", "this", "is", "good", "I", "think", "this", "looks", "good",
@@ -5,23 +7,65 @@ const WORD_SET_EN: &[&str] = &[
     "I", "want", "to", "drink", "water", "it", "is", "rainy", "here"
 ];
 
-pub fn run(words: usize, count: usize, random: bool) -> anyhow::Result<()> {
+const WORD_SET_JA: &[&str] = &[
+    "こんにちは", "元気", "ですか", "今日", "私", "は", "元気", "です",
+    "あなた", "は", "どう", "ですか", "これ", "は", "いい", "と思う",
+    "これ", "は", "良さそう", "私たち", "は", "とても", "暇です",
+    "何", "ですか", "これ", "私", "は", "水", "を", "飲みたい", "ここ", "は", "雨", "です"
+];
+
+const WORD_SET_DE: &[&str] = &[
+    "hallo", "wie", "geht", "es", "dir", "heute", "ich", "bin", "gut",
+    "und", "du", "dies", "ist", "gut", "ich", "denke", "dies", "sieht", "gut", "aus",
+    "lass", "uns", "etwas", "neues", "versuchen", "was", "ist", "dies",
+    "ich", "möchte", "trinken", "wasser", "es", "regnet", "hier"
+];
+
+const WORD_SET_FR: &[&str] = &[
+    "bonjour", "comment", "ça", "va", "aujourd'hui", "je", "vais", "bien",
+    "et", "toi", "ceci", "est", "bon", "je", "pense", "cela", "semble", "bien",
+    "laissons", "nous", "essayer", "quelque", "chose", "de", "nouveau", "que", "est", "ceci",
+    "je", "veux", "boire", "de", "l'eau", "il", "pleut", "ici"
+];
+
+const WORD_SET_ES: &[&str] = &[
+    "hola", "cómo", "estás", "hoy", "yo", "estoy", "bien",
+    "y", "tú", "esto", "es", "bueno", "creo", "que", "esto", "parece", "bien",
+    "vamos", "a", "probar", "algo", "nuevo", "qué", "es", "esto",
+    "quiero", "beber", "agua", "aquí", "está", "lloviendo"
+];
+
+#[derive(Debug, Clone, ValueEnum, PartialEq, Copy)]
+pub enum Lang { En, Ja, De, Fr, Es }
+
+pub fn run(words: usize, count: usize, random: bool, lang: Option<Lang>) -> anyhow::Result<()> {
     let mut results: Vec<String> = Vec::with_capacity(count);
+    let actual_lang = lang.unwrap_or(Lang::En);
     
     for _ in 0..count {
-        results.push(generate_text(WORD_SET_EN, words, random));
+        results.push(generate_text(words, random, actual_lang));
     }
     
     println!("{}", results.join("\n\n"));
     Ok(())
 }
 
-fn generate_text(word_set: &[&str], words: usize, random: bool) -> String {
+fn generate_text(words: usize, random: bool, lang: Lang) -> String {
+    let word_set: &[&str] = match lang {
+        Lang::En => WORD_SET_EN,
+        Lang::Ja => WORD_SET_JA,
+        Lang::De => WORD_SET_DE,
+        Lang::Fr => WORD_SET_FR,
+        Lang::Es => WORD_SET_ES
+    };
+    
+    let separator = if lang == Lang::Ja { "" } else { " " };
+    
     if random {
-        let mut word_set_vec = word_set.to_vec();
+        let mut word_set_vec = Vec::from(word_set);
         fastrand::shuffle(&mut word_set_vec);
-        word_set_vec.iter().cycle().take(words).copied().collect::<Vec<_>>().join(" ")
+        word_set_vec.iter().cycle().take(words).copied().collect::<Vec<_>>().join(separator)
     } else {
-        word_set.iter().cycle().take(words).copied().collect::<Vec<_>>().join(" ")
+        word_set.iter().cycle().take(words).copied().collect::<Vec<_>>().join(separator)
     }
 }
