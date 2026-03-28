aa51d64e165c6ed512369ee3b4800cce37944a7b
mylu0x <229919353+mylu0x@users.noreply.github.com>
2026-03-28 11:22:39 +0900

    feat(lorem): add generate logic

--- src/commands/lorem.rs
+++ src/commands/lorem.rs
@@ -1,5 +1,13 @@
 use anyhow::Ok;
 
+use crate::data::word_sets::WORD_SET_LOREM;
+
 pub fn run() -> anyhow::Result<()> {
+    println!("{}", generate_lorem());
     Ok(())
+}
+
+fn generate_lorem() -> String {
+    let word_set = WORD_SET_LOREM;
+    word_set.iter().copied().collect::<Vec<_>>().join(" ")
 }
