476bca4371aec324c734cc5c62f56210a45372a9
mylu0x <229919353+mylu0x@users.noreply.github.com>
2026-03-28 11:17:53 +0900

    chore(word_sets): add add Lorem word set

--- src/data/word_sets.rs
+++ src/data/word_sets.rs
@@ -60,3 +60,15 @@ pub(crate) const WORD_SET_IT: &[&str] = &[
     "proviamo", "qualcosa", "di", "nuovo", "che", "è", "questo",
     "io", "voglio", "bere", "acqua", "è", "piovoso", "qui"
 ];
+
+pub(crate) const WORD_SET_LOREM: &[&str] = &[
+    "Lorem", "ipsum", "dolor", "sit", "amet,", "consectetur", "adipiscing", "elit,", 
+    "sed", "do", "eiusmod", "tempor", "incididunt", "ut", "labore", "et", "dolore", 
+    "magna", "aliqua.", "Ut", "enim", "ad", "minim", "veniam,", "quis", "nostrud", 
+    "exercitation", "ullamco", "laboris", "nisi", "ut", "aliquip", "ex", "ea", 
+    "commodo", "consequat.", "Duis", "aute", "irure", "dolor", "in", "reprehenderit", 
+    "in", "voluptate", "velit", "esse", "cillum", "dolore", "eu", "fugiat", "nulla", 
+    "pariatur.", "Excepteur", "sint", "occaecat", "cupidatat", "non", "proident,", 
+    "sunt", "in", "culpa", "qui", "officia", "deserunt", "mollit", "anim", "id", 
+    "est", "laborum."
+];
