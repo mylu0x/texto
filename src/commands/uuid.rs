85462f99c65aa029f63c2180be0091b30f79306b
mylu0x <229919353+mylu0x@users.noreply.github.com>
2026-03-29 11:23:36 +0900

    feat(uuid): add v4 uuid generate logic

--- src/commands/uuid.rs
+++ src/commands/uuid.rs
@@ -1,3 +1,11 @@
+use uuid::Uuid;
+
 pub fn run() -> anyhow::Result<()> {
+    println!("{}", generate_uuid());
     Ok(())
+}
+
+fn generate_uuid() -> String {
+    let result = Uuid::new_v4().to_string();
+    result
 }
